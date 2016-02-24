26669817bb3a80d6009d7b6e1f2a5f5ccc28e823
youngsterxyf <sas.198708@gmail.com>
2016-02-24 18:39:53 +0800

    init

---
+++ Formula/bee.rb
@@ -0,0 +1,17 @@
+class Bee < Formula
+    homepage "https://github.com/beego/bee"
+    desc "Bee is a tool for helping develop with beego app framework."
+
+    head "https://github.com/beego/bee.git"
+
+    def install
+        system "mkdir" "-p" "#{buildpath}/src/github.com/beego/bee"
+        system "cp" "-r" "./*" "#{buildpath}/src/github.com/beego/bee"
+        ENV["GOPATH"] = buildpath
+
+        system "go" "install" "github.com/beego/bee"
+
+        system "cp" "#{buildpath}/bin/bee" "#{bin}/"
+
+        (HOMEBREW_PREFIX/bin).install_symlink "#{bin}/bee"
+end
